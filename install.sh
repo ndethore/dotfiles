2c8d8e78ebcd19d569eb9f12b753b6728aa3409d
Nicolas <nicolas.dethore@gmail.com>
2018-10-25 01:48:14 +0900

    Add install script that generate local config files sourcing the base, versionned ones

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo
+echo -n "Would you like to backup your current dotfiles? (y/n) "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+	mv ~/.zshrc ~/.zshrc.old
+	mv ~/.tmux.conf ~/.tmux.conf.old
+	mv ~/.vimrc ~/.vimrc.old
+else
+	echo -e "\nNot backing up old dotfiles."
+fi
+
+printf "source '$HOME/.dotfiles/zsh/zshrc'" > ~/.zshrc
+printf "so $HOME/.dotfiles/vim/vimrc" > ~/.vimrc
+printf "so $HOME/.dotfiles/vim/vimrc" > ~/.config/nvim/init.vim
+printf "source-file $HOME/.dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+
+echo
+echo "Please log out and log back in for default shell to be initialized."
